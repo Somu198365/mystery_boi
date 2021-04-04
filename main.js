f40b4f4e568751ce6ce6034d3c2f058b22f8a0ad
Somu198365 <73246101+Somu198365@users.noreply.github.com>
2021-04-04 11:22:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,16 +12,20 @@ function setup()
 
 function draw()
 {
-    image(video,0,0,640,480);
+    image(video,230,150,220,200);
     tint("orange");
     fill(255,0,0);
     stroke(255,0,0);
-    circle(50,50,70);
+    circle(50,50,80);
 
     fill(0,128,0);
     stroke(0,128,0);
     rect(50,40,460,20)
+    rect(90,420,460,20)
+    fill(0,178,0);
+    stroke(0,138,0);
     rect(50,40,460,20)
+    rect(90,420,460,20)
 }
 
 function take_snapshot()
